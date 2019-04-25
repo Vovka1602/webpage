25c52c3341f02dd5c7b9a4d65161b27370b96922
Vovka1602 <48223638+Vovka1602@users.noreply.github.com>
2019-04-25 13:21:49 +0300

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+let x = 3;
+let y = 5;
+
+console.log("x", x, "y", y);
+
+let sum = x+y;
+let difference = x-y;
+let quotient = x/y;
+console.log(sum, difference, quotient);
+
+let name = "Володимир";
+let lastname = "Калашников";
+let fullname = name + " " + lastname + "2004";
+console.log(fullname);
+console.log(fullname + 2004);
+
